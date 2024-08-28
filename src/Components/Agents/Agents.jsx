c3a3860b37fc48ebb2d2b02b66b119a6bfe8be3f
DineshJrr <jrrdinesh2107@gmail.com>
2024-08-28 07:00:26 +0545

    agent top section completed

--- src/Components/Agents/Agents.jsx
+++ src/Components/Agents/Agents.jsx
@@ -1,31 +1,30 @@
 import React from "react";
-import './Agents.css'
-import Box from '../../Components/Agents/Box'
+import "./Agents.css";
+import Box from "../../Components/Agents/Box";
+import agents from '../../assets/agents.png'
 
-
-function Agents(){
-    return(
-        <div className="agents">
-            <div className="agents-top">
-            <h4>EXPERTISE IS HERE</h4>
-            <h3>Our Growing Agents</h3>
-            <div className="top-container">
-            <Box></Box>           
-            <Box></Box>           
-            <Box></Box> 
-            <Box></Box> 
-            </div>
-                     
-            </div>
-            <div className="agents-lower">
-                <img src="" alt="" />
-                <div className="lower-content">
-                    <h4>Become An Agent</h4>
-                    <p>Agent hen an unknown printer took a gallery scramble</p>
-                </div>
-                <button>Join Now</button>
-            </div>
+function Agents() {
+  return (
+    <div className="agents">
+      <div className="agents-top">
+        <h4>EXPERTISE IS HERE</h4>
+        <h3>Our Growing Agents</h3>
+        <div className="top-container">
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
         </div>
-    )
+      </div>
+      <div className="agents-lower">
+        <img src={agents} alt="" />
+        <div className="lower-content">
+          <h4>Become An Agent</h4>
+          <p>Agent hen an unknown printer took a gallery scramble</p>
+        </div>
+        <button>Join Now</button>
+      </div>
+    </div>
+  );
 }
-export default Agents
+export default Agents;
