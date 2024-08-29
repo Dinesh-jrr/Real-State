3d1c5e47345f6aee0c637ee9e80278a9640a931b
DineshJrr <jrrdinesh2107@gmail.com>
2024-08-29 06:24:13 +0545

    features section completed

---
+++ src/Components/Features/Box.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './Features.css'
+import Info from '../../Components/Features/Info';
+import room from "../../assets/room.png";
+import location from '../../assets/location.png';
+import clock from '../../assets/clock.png';
+import user from '../../assets/user.png';
+
+function Box(){
+    return(
+        <div className="features-container">
+          <div className="top-img">
+            <img src={room} alt="" />
+            <p>For Sale</p>
+          </div>
+          <h3>1963 S Crescent Heights Blvd</h3>
+          <div className="container-location">
+            <img src={location} alt="" />
+            <h5>Hills,CA 90210</h5>
+          </div>
+            <hr />
+            <div className="container-middle">
+                <Info></Info>
+                <Info></Info>
+                <Info></Info>
+                <Info></Info>
+        </div>
+          <div className="container-lower">
+            <div className="lower-name">
+              <img src={user} alt="" />
+              <h4>Michel Smith</h4>
+            </div>
+            <div className="lower-time">
+              <img src={clock}alt="" />
+              <h4>1 days ago</h4>
+            </div>
+          </div>
+          <button className="container-button">$5,304,000</button>
+        </div>
+    )
+}
+export default Box
--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,45 +1,18 @@
 import React from "react";
 import "./Features.css";
-import room from "../../assets/room.png";
-import location from '../../assets/location.png'
-import clock from '../../assets/clock.png'
-import user from '../../assets/user.png'
-import Info from '../../Components/Features/Info'
+import Box from '../../Components/Features/Box'
 function Features() {
   return (
     <div className="features">
       <h4 className="features-title">FEATURED LISTING</h4>
       <h3 className="features-info">We Bring Dream Homes To Reality</h3>
       <div className="features-content">
-        <div className="features-container">
-          <div className="top-img">
-            <img src={room} alt="" />
-            <p>For Sale</p>
-          </div>
-          <h3>1963 S Crescent Heights Blvd</h3>
-          <div className="container-location">
-            <img src={location} alt="" />
-            <h5>Hills,CA 90210</h5>
-          </div>
-            <hr />
-            <div className="container-middle">
-                <Info></Info>
-                <Info></Info>
-                <Info></Info>
-                <Info></Info>
-        </div>
-          <div className="container-lower">
-            <div className="lower-name">
-              <img src={user} alt="" />
-              <h4>Michel Smith</h4>
-            </div>
-            <div className="lower-time">
-              <img src={clock}alt="" />
-              <h4>1 days ago</h4>
-            </div>
-          </div>
-          <button className="container-button">$5,304,000</button>
-        </div>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
       </div>
     </div>
   );
