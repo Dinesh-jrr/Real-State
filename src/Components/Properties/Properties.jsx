9872befa1b087f956b050a61ddecb79dd9e94bb3
DineshJrr <jrrdinesh2107@gmail.com>
2024-08-30 06:26:18 +0545

    html structured completed

--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -12,6 +12,23 @@ function Properties(){
                 <h4>$300,000</h4>
                 <p>5232 North Carolina Ave 21BC</p>
                 <h4>California,USA</h4>
+                <div className="properties-content">
+                    <div className="content-item">
+                        <img src="" alt="" />
+                        <p>4 beds</p>
+                    </div>
+                    <div className="content-item">
+                        <img src="" alt="" />
+                        <p>4 beds</p>
+                    </div>
+                </div>
+                <div className="container-lower">
+                    <img src="" alt="" />
+                    <div className="lower-content">
+                        <h4>William Hue</h4>
+                        <p>Estate Agents</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
